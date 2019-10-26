5ab30d64e7c622b638641e109131fe8957f56e58
Rafael Rocha <rafarocha.webdev@gmail.com>
2019-10-26 17:51:52 +0200

    add basic calculator layout and set default display value

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-const name = 'Rafael';
+const displayElement = document.querySelector('.display');
+// set default calculator display value to 0.
+displayElement.innerHTML = '0.';
+const displayElementNumericalValue = parseFloat(displayElement.innerHTML);
 
-console.log(name);
+console.log(displayElementNumericalValue);
